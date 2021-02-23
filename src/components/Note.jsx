c5fe4e84ac5f08777ef1dc6efef235ba5d3d9c79
vintvgx <69157141+vintvgx@users.noreply.github.com>
2021-02-23 01:59:20 -0500

    corrections

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,13 +7,14 @@ function Note() {
   return (
     <div className="pageTitle container-fluid">
       <h1>iPhone 13</h1>
-      <h4 style={{marginBottom: "15px"}}>"Full Screen. that you can feel"</h4>
+      <h4 style={{marginBottom: "15px"}}>"Full Screen"</h4>
       <p>From $41.62/mo. or $999 before trade-in.</p>
       <p style={{marginBottom: "15px"}}>Buy directly from Apple with special carrier offers.</p>
-      <a href="apple.com">Learn More >  </a>
-      <a href="apple.com">Buy ></a>
+      <a href="apple.com">Learn More {">"}  </a>
+      <a href="apple.com">Buy {">"}</a>
       <div>
         <img className="iPic" src="./pics/iphone13.png" alt="IPHONE!#"/>
+        <img className="iPic" src="../public/pics/iphone13.png" alt="IPHONE!#"/>
       </div>
     </div>
   );
---
+++ src/components/Options.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Options () {
+    return (
+        <div>
+            <p>lets get into the iphone options using map</p>
+        </div>
+    )
+}
+
+export default Options;
